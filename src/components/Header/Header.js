456581444dda7fbaf7e58afb1ea392d3b4139e92
fardinkhan69 <fardinofficial69@gmail.com>
2021-03-01 21:07:45 +0600

    all done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,10 @@ import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 const Header = () => {
     return (
         <div>
+            <div className="text-center">
+                <h2>BRFC Football League</h2>
+                <h5>Choose the best players to make the best team</h5>
+            </div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <a className="navbar-brand" href="">Navbar</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,9 +16,9 @@ const Header = () => {
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
                         <a className="nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
-                        <a className="nav-link" href="#">Features</a>
-                        <a className="nav-link" href="#">Pricing</a>
-                        <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        <a className="nav-link" href="/players">Players</a>
+                        <a className="nav-link" href="/goalkeeper">goalkeepers</a>
+                        
                     </div>
                 </div>
             </nav>
